bd1b7129df06e5ff21cde941a174622e1c159571
Omar <gomar@dmacc.edu>
2019-09-17 10:49:58 -0700

    second test passed

--- test_ input_validation/test_validation_with_if.py
+++ test_ input_validation/test_validation_with_if.py
@@ -11,10 +11,9 @@ import unittest.mock as result
 class MyTestCase(unittest.TestCase):
 
     def test_average(self):
-        with result.patch('builtins.input', side_effect=[-1, 2, 2]):
-            assert validation_with_if.average() == 1
-            with result.patch('builtins.input', side_effect=[-1, 2, 2]):
-                assert validation_with_if.average() == 1
+        with result.patch('builtins.input', side_effect=[-1, -2, 2]):
+            assert validation_with_if.average() == -0.3333333333333333
+
 
 
 if __name__ == '__main__':
